7ee6eedaa37ae554fb8c6fbf39b279837ba84698
muskan <muskan8878h@gmail.com>
2025-03-11 17:53:34 +0530

    updated

--- jaaaaaaaaa/doublLL.java
+++ jaaaaaaaaa/doublLL.java
@@ -38,6 +38,29 @@ public class doublLL {
         }
         System.out.println();
     }
+    public void reversedisplay(){
+        Node current = tail;
+        if(tail==null){
+            System.out.println("No elements in the list");
+            return ;
+        }
+        System.out.println("Node of doubly Linked list");
+        while(current!=null){
+            System.out.print(current.data+" ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){ 
+        doublLL dll = new doublLL();
+        dll.addNode(10);
+        dll.addNode(35);
+        dll.addNode(57);
+        dll.addNode(24);
+        dll.addNode(49);
+        dll.display();
+        dll.reversedisplay();
+    } 
 
-    
+
 }
